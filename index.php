028379a111f47ffd4e640ada0ba6d2dec9d22545
lgbeauty <salaolgbeauty@gmail.com>
2013-04-01 15:45:43 -0300

    ie7 improvements and corrections - 1

--- index.php
+++ index.php
@@ -74,168 +74,169 @@
                 <li><a href="/es/"><figure><img src="/i/es.png" alt="Español" /><figcaption>ES</figcaption></figure></a></li>
             </ul></li>
         </ul></nav>
+</header>
 
-    <div id="main">
-        <section id="fotos">
-            <div class="mask"><img src="/i/i1.jpg" /><img src="/i/i2.jpg" /><img src="/i/i3.jpg" /><img src="/i/i4.jpg" /><img src="/i/i5.jpg" /><img src="/i/i1.jpg" /></div>
-        </section>
+<div id="main">
+    <section id="fotos">
+        <div class="mask"><img src="/i/i1.jpg" /><img src="/i/i2.jpg" /><img src="/i/i3.jpg" /><img src="/i/i4.jpg" /><img src="/i/i5.jpg" /><img src="/i/i1.jpg" /></div>
+    </section>
 
-        <?php echo $body; ?>
+    <?php echo $body; ?>
 
-        <div id="localizacao">
+    <div id="localizacao">
 
-                <h3>Tecsis Tecnologia e Sistemas Avançados</h3>
+        <h3>Tecsis Tecnologia e Sistemas Avançados</h3>
 
-                <div id="hcard-Tecsis-Planta-1" class="vcard">
-                    <h4 class="fn">Planta 1</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">rua Moacyr Ozeas Guitti, 36 <br> Zona Industrial</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span> 
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18086-390</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-1" class="vcard">
+            <h4 class="fn">Planta 1</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">rua Moacyr Ozeas Guitti, 36 <br> Zona Industrial</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span> 
+                <span class="region">SP</span><br>
+                <span class="postal-code">18086-390</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-3" class="vcard">
-                    <h4 class="fn">Planta 3</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">rua Anuar Dequech, 215, Lote C/D-2 <br>Iporanga I</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span>  
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-157</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-3" class="vcard">
+            <h4 class="fn">Planta 3</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">rua Anuar Dequech, 215, Lote C/D-2 <br>Iporanga I</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span>  
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-157</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-7" class="vcard">
-                    <h4 class="fn">Planta 7</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">av. Jerome Case, 3000 <br> Éden</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span> 
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-200</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-7" class="vcard">
+            <h4 class="fn">Planta 7</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">av. Jerome Case, 3000 <br> Éden</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span> 
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-200</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-8" class="vcard">
-                    <h4 class="fn">Planta 8</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">av. Hollingsworth, 215 <br> Éden</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span> 
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-105</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-8" class="vcard">
+            <h4 class="fn">Planta 8</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">av. Hollingsworth, 215 <br> Éden</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span> 
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-105</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-9" class="vcard">
-                    <h4 class="fn">Planta 9</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">av. Jerome Case, 2400 <br> Éden</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span> 
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-220</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-9" class="vcard">
+            <h4 class="fn">Planta 9</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">av. Jerome Case, 2400 <br> Éden</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span> 
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-220</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-10" class="vcard">
-                    <h4 class="fn">Planta 10</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">rod. Raposo Tavares, s/nº, km 105 + 240m Itanguá</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span>
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18203-340</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-10" class="vcard">
+            <h4 class="fn">Planta 10</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">rod. Raposo Tavares, s/nº, km 105 + 240m Itanguá</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span>
+                <span class="region">SP</span><br>
+                <span class="postal-code">18203-340</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-11" class="vcard">
-                    <h4 class="fn">Planta 11</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">av. Três de Março, 6510 <br> Aparecidinha</span><span>,</span>
-                        <span class="locality">Sorocaba</span><span>,</span>  
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-275</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-11" class="vcard">
+            <h4 class="fn">Planta 11</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">av. Três de Março, 6510 <br> Aparecidinha</span><span>,</span>
+                <span class="locality">Sorocaba</span><span>,</span>  
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-275</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-12" class="vcard">
-                    <h4 class="fn">Planta 12</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">av. John Boyd Dunlop, 265 <br> Iporanga</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span> 
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-155</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-12" class="vcard">
+            <h4 class="fn">Planta 12</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">av. John Boyd Dunlop, 265 <br> Iporanga</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span> 
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-155</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-13" class="vcard">
-                    <h4 class="fn">Planta 13</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">rodovia Waldomiro Correa Camargo, s/nº, km 60.5, Pirapitingui</span><span>,</span> 
-                        <span class="locality">Itu</span><span>,</span> 
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">13308-200</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-13" class="vcard">
+            <h4 class="fn">Planta 13</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">rodovia Waldomiro Correa Camargo, s/nº, km 60.5, Pirapitingui</span><span>,</span> 
+                <span class="locality">Itu</span><span>,</span> 
+                <span class="region">SP</span><br>
+                <span class="postal-code">13308-200</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-CDM" class="vcard">
-                    <h4 class="fn">Planta CDM</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">rua Seike Murakami, 211 <br> Aparecidinha</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span> 
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-275</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-CDM" class="vcard">
+            <h4 class="fn">Planta CDM</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">rua Seike Murakami, 211 <br> Aparecidinha</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span> 
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-275</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-14" class="vcard">
-                    <h4 class="fn">Planta 14</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">av. Hollingsworth, 1046 <br> Iporanga</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span>
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18087-105</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
+        <div id="hcard-Tecsis-Planta-14" class="vcard">
+            <h4 class="fn">Planta 14</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">av. Hollingsworth, 1046 <br> Iporanga</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span>
+                <span class="region">SP</span><br>
+                <span class="postal-code">18087-105</span>
+                <span class="country-name">Brasil</span>
+            </address>
+        </div>
 
-                <div id="hcard-Tecsis-Planta-15" class="vcard">
-                    <h4 class="fn">Planta 15</h4>
-                    <span class="org">Tecsis</span>
-                    <address class="adr">
-                        <span class="street-address">av. Victor Andrew, 1250 <br> Zona Industrial</span><span>,</span> 
-                        <span class="locality">Sorocaba</span><span>,</span>
-                        <span class="region">SP</span><br>
-                        <span class="postal-code">18086-390</span>
-                        <span class="country-name">Brasil</span>
-                    </address>
-                </div>
-                
-                <br style="clear:both" />
-
-
+        <div id="hcard-Tecsis-Planta-15" class="vcard">
+            <h4 class="fn">Planta 15</h4>
+            <span class="org">Tecsis</span>
+            <address class="adr">
+                <span class="street-address">av. Victor Andrew, 1250 <br> Zona Industrial</span><span>,</span> 
+                <span class="locality">Sorocaba</span><span>,</span>
+                <span class="region">SP</span><br>
+                <span class="postal-code">18086-390</span>
+                <span class="country-name">Brasil</span>
+            </address>
         </div>
         
         <br style="clear:both" />
+
+
+    </div>
+        
+    <br style="clear:both" />
     </div>
 
 </footer>
